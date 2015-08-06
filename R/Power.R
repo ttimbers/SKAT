ec56553ddb2cc6a7265924dd4f65787268b0741e
ttimbers <tiffany.timbers@gmail.com>
2015-08-06 15:34:38 -0700

    OK - I dug through the code and I think I figured out what was wrong - sampling for SNPs in regions where there are no SNPs. Since my haplotype matrix comes from real data, I don't have control over how uniform it is, and on the right-hand side of the Chromosome (see figure) I have very few SNPs. Thus when Get_RandomRegion() is called and it ends up in the sparse SNP region, IDX becomes empty and then Power_Logistic() throws the error: Error in Get_CausalSNPs(Marker.MAF, Causal.Percent/100, Causal.MAF.Cutoff) : No SNPs with MAF < 0.030000. The fix for this is to add a while loop to Get_RandomRegion() so that it keeps running the running the function until IDX is not empty (length != 0).

--- R/Power.R
+++ R/Power.R
@@ -369,19 +369,20 @@ Get_RandomRegion<-function(SNP.Dist,SubRegion.Length){
 	if(SubRegion.Length < 0){
 		return(1:length(SNP.Dist))
 	}
-	total.first<-min(SNP.Dist)
-	total.last<-max(SNP.Dist)
-	total.length<-total.last -total.first
+	while(length(IDX) == 0){
+		total.first<-min(SNP.Dist)
+		total.last<-max(SNP.Dist)
+		total.length<-total.last -total.first
 	
-	Region.Start<-runif(1) * (total.length - SubRegion.Length)  + total.first
-	Region.End<-Region.Start + SubRegion.Length
+		Region.Start<-runif(1) * (total.length - SubRegion.Length)  + total.first
+		Region.End<-Region.Start + SubRegion.Length
 
-	#print(c(Region.Start,Region.End,total.first,total.last))
+		#print(c(Region.Start,Region.End,total.first,total.last))
 
-	Marker.Idx1<-which(SNP.Dist >= Region.Start)
-	Marker.Idx2<-which(SNP.Dist <= Region.End)
-	IDX<-sort(intersect(Marker.Idx1,Marker.Idx2))
-	
+		Marker.Idx1<-which(SNP.Dist >= Region.Start)
+		Marker.Idx2<-which(SNP.Dist <= Region.End)
+		IDX<-sort(intersect(Marker.Idx1,Marker.Idx2))
+	}
 	return(IDX)
 
 }
